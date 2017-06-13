e4e04bb4d39be8f4cf8cb2f9ce818de06d11558f
Jeremy <jneveu@lal.in2p3.fr>
2017-06-13 06:31:13 +0200

    Analysis of Thor300 and Ron400 Halpha scans

---
+++ common_tools/tools.py
@@ -0,0 +1,10 @@
+from scipy.optimize import curve_fit
+import numpy as np
+
+def gauss(x,a,x0,sigma):
+    return a*np.exp(-(x-x0)**2/(2*sigma**2))
+
+def fit_gauss(x,y,guess=[10,1000,1],bounds=(-np.inf,np.inf)):
+    popt,pcov = curve_fit(gauss,x,y,p0=guess,bounds=bounds)
+    return popt, pcov
+
